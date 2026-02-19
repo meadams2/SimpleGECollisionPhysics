969f0f6f17d685dd2e6b93455d01dc070dba6573
Marianne Adams <meadams2@bsu.edu>
2026-02-18 22:24:02 -0500

    SAT with testing
    Separating Axis Theorem Collisions work with testing frame. Works with rotation and without. All of pseudocode and math was done primarily on sheets of scrap paper due to limited availability of rooms with huge whiteboards

--- CollisionPhysics.py
+++ CollisionPhysics.py
@@ -2,43 +2,125 @@
 Marianne Adams, 2025
 Created to be used in conjunction with SimpleGE for more advanced collisions"""
 
-import simpleGE, pygame
+import simpleGE
+from pygame.math import Vector2
 
 class CollisionPhysics(simpleGE.Sprite):
+    def __init__(self, scene):
+        super().__init__(scene)
+        self.originPolygon = []
     
     def collidesWithAdvanced(self, target):
         """Boolean function. Returns True if the sprite
            is currently colliding with the target sprite,
-           false otherwise."""
-        collision = False
-        if self.visible:
-            if target.visible:
-                polygonSprite = spriteToPolygon(self)
-                polygonTarget = spriteToPolygon(target)
-                
-                for polygon in (polygonSprite, polygonTarget):
-                    edges = getEdges(polygon)
-                    
-                    for edge in edges:
-                        axis = getLeftNormal(edge)
-                        
-                        minSprite, maxSprite = getProjection(polygonSprite, axis)
-                        minTarget, maxTarget = getProjection(polygonTarget, axis)
-                        
-                        # If there's a separating axis, no collision
-                        if maxSprite < minTarget:
-                            collision = False
-                        if maxTarget < minSprite:
-                            collision = False
-        return collision
+           false otherwise.
+           Utilizes SAT collision detection"""
+        # Check visibility
+        if not self.visible or not target.visible:
+            return False
+        
+        # Convert sprite edges to geometric vectors
+        spritePolygon = self.spriteToPolygon()
+        targetPolygon = target.spriteToPolygon()
+        
+        # Get edges from both shapes
+        spriteEdges = self.getEdges(spritePolygon)
+        targetEdges = self.getEdges(targetPolygon)
+        
+        for edge in spriteEdges + targetEdges:
+            # Get perpendicular axis
+            axis = self.getNormal(edge)
+            
+            # Project both polygons onto axis
+            minSprite, maxSprite = self.getProjection(spritePolygon, axis)
+            minTarget, maxTarget = self.getProjection(targetPolygon, axis)
+            
+            # Check for separating axis
+            if maxSprite < minTarget:
+                return False
+            if maxTarget < minSprite:
+                return False
+            
+        return True
+        
+        
+    def buildRectangularPolygon(self):
+        """Gets Rectangular polygon based on imageMaster.
+           Helper function.
+           Centered at origin."""
+        width = self.imageMaster.get_width()
+        height = self.imageMaster.get_height()
+        
+        self.originPolygon = [
+            Vector2(-width/2, -height/2),
+            Vector2(width/2, -height/2),
+            Vector2(width/2, height/2),
+            Vector2(-width/2, height/2)]
     
     def spriteToPolygon(self):
-        """Handles n edges to handle more complex shapes"""
+        """Rectangular polygon is rotated by the image angle and shifted by x and y.
+           Sprite's actual screen position & represents real shape
+           Geometry conversion"""
         
-        sprite_width = self.imageMaster.get_width()
-        sprite_height = self.imageMaster.getHeight()
+        if len(self.originPolygon) == 0:
+            self.buildRectangularPolygon()
+        
+        polygonEdgePoints = []
+        
+        for point in self.originPolygon:
+            rotated = point.rotate(-self.imageAngle)
+            # Calculate point not centered at origin
+            calculatedPoint = rotated + Vector2(self.x, self.y)
+            polygonEdgePoints.append(calculatedPoint)
+        
+        return polygonEdgePoints
+    
+    def getEdges(self, polygon):
+        """Extracts side vectors to from sprite.
+           Determines direction vectors of the polygon's sides
+           Assumes polygon is convex"""
+        edges = []
+        numberEdges = len(polygon)
+        
+        for i in range(numberEdges):
+            point1 = polygon[i]
+            point2 = polygon[(i+1) % numberEdges] # Wraps around to get 1st point
+            edge = point2 - point1
+            edges.append(edge)
+        
+        return edges
+    
+    def getNormal(self, edge):
+        """Gets the normal vector for the given edge."""
+        normal = edge.rotate(90)
+        if normal.length() != 0:
+            normal = normal.normalize() # Method comes from pygame.math.Vector2
+            
+        return normal
+    
+    def getProjection(self, polygon, axis):
+        """The polygon is a list of Vector2 points
+           The axis is a normalized vector
+           Projects every vertex of a polygon onto a single axis
+           Returns minimum and maximum scalar values
+           Uses dot product of vectors to get orthogonal projection"""
+        minimumProjection = polygon[0].dot(axis)
+        maximumProjection = minimumProjection
+        
+        for point in polygon[1:]:
+            projection = point.dot(axis)
+            
+            if projection < minimumProjection:
+                minimumProjection = projection
+            elif projection > maximumProjection:
+                maximumProjection = projection
+        
+        return minimumProjection, maximumProjection
         
     
+            
+        
+        
         
         
     
--- testCollisionPhysics.py
+++ testCollisionPhysics.py
@@ -2,80 +2,57 @@
 Testing Frame for Collision Physics
 Marianne Adams, 2025"""
 
-import collisionPhysics, simpleGE, pygame
+import CollisionPhysics, simpleGE, pygame
 
-class MovingObject(simpleGE.Sprite):
-    def __init__(self, scene):
+class TestSprite(CollisionPhysics.CollisionPhysics):
+    def __init__(self, scene, color, size, x, y, angle=0, active=False):
         super().__init__(scene)
-        self.colorRect("Green", (50, 50))
-        
-    def process(self):
-        """Tests movement, checks for barrier collisions and calls collisionPhysics accordingly"""
-
+        self.colorRect(color, size)
+        self.x = x
+        self.y = y
+        self.setAngle(angle)
+        self.moveAngle = angle
         self.speed = 0
-
-        # Movement
-        if self.isKeyPressed(pygame.K_a):
-            self.dx = -5
-            self.dir = "left"
-        elif self.isKeyPressed(pygame.K_d):
-            self.dx = 5
-            self.dir = "right"
-        elif self.isKeyPressed(pygame.K_w):
-            self.dy = -5
-            self.dir = "up"
-        elif self.isKeyPressed(pygame.K_s):
-            self.dy = 5
-            self.dir = "down"
-
-        # Barrier Checking
-        barrier = self.scene.barrier
-        if self.collidesWith(barrier):
-            collisionResult = collisionPhysics.handleCollisionOfTwoObjects(self, barrier)
-        
-    class DrivingObject(simpleGE.Sprite):
-        def __init__(self, scene):
-            super().__init__(scene)
-            self.colorRect("blue", (50, 20))
-            self.setAngle(45)
-        
-        def process(self):
-            
-            """Tests angle of reflection when colliding against barrier"""
-            
-            if self.isKeyPressed(pygame.K_UP):
-                self.speed += .1
-            if self.isKeyPressed(pygame.K_DOWN):
-                self.speed -= .1
+        self.active = active
+    
+    def process(self):
+        # Allow for movement of first sprite only to test
+        if self.active:
             if self.isKeyPressed(pygame.K_LEFT):
-                self.imageAngle += 5
-                self.moveAngle += 5
+                self.x -= 3
             if self.isKeyPressed(pygame.K_RIGHT):
-                self.imageAngle -= 5
-                self.moveAngle -= 5
-            
-            if self.isKeyPressed(pygame.K_w):
-                self.boundAction = self.WRAP
-            elif self.isKeyPressed(pygame.K_b):
-                self.boundAction = self.BOUNCE
-            
-            self.scene.lblOut.text = f"m: {self.moveAngle}, i: {self.imageAngle}"
-            
-            barrier = self.scene.barrier
-            angle = self.moveAngle % 360
-            
-            if angle < 45:
-                direction = "right"
-            elif angle <135:
-                direction = "up"
-            elif angle < 225:
-                direction = "left"
-            elif angle < 315:
-                direction = "down"
-            else:
-                direction = "right"
-                
-            if self.collidesWith(barrier):
-                collisionResult = collisionPhysics.handleAngleOfCollision(self, barrier)
-            
-            
+                self.x += 3
+            if self.isKeyPressed(pygame.K_UP):
+                self.y -= 3
+            if self.isKeyPressed(pygame.K_DOWN):
+                self.y += 3
+        
+        # Rotation
+            if self.isKeyPressed(pygame.K_q):
+                self.imageAngle += 3
+                self.moveAngle += 3
+            if self.isKeyPressed(pygame.K_e):
+                self.imageAngle -= 3
+                self.moveAngle -= 3
+
+class CollisionScene(simpleGE.Scene):
+    def __init__(self):
+        super().__init__()
+        self.setCaption("SAT Collision Test")
+        
+        self.sprite1 = TestSprite(self, "red", (120, 60), 300, 300, 30, active=True)
+        self.sprite2 = TestSprite(self, "blue", (100, 40), 500, 200, -20)
+        
+        self.sprites = [self.sprite1, self.sprite2]
+        
+    def update(self):
+        super().update()
+        
+        testSAT = self.sprite1.collidesWithAdvanced(self.sprite2)
+        testSimple = self.sprite1.collidesWith(self.sprite2)
+        
+        print(f" SAT: {testSAT} | Simple {testSimple}")
+        
+if __name__ == "__main__":
+    scene = CollisionScene()
+    scene.start()
