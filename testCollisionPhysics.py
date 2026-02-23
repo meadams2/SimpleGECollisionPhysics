4be0bc42541e0d359c7f357a31c99f0e9b9d0219
Marianne Adams <meadams2@bsu.edu>
2026-02-22 21:59:08 -0500

    Digitized Algorithm Files and Reword Testing
    Currently only looking at the sprite1, but need to work to check both sprite1 and sprite2. Will do in morning when less exhausted.

--- testCollisionPhysics.py
+++ testCollisionPhysics.py
@@ -4,8 +4,8 @@ Marianne Adams, 2025"""
 
 import CollisionPhysics, simpleGE, pygame
 
-class TestSprite(CollisionPhysics.CollisionPhysics):
-    def __init__(self, scene, color, size, x, y, angle=0, active=False):
+class MoveSprite(CollisionPhysics.CollisionPhysics):
+    def __init__(self, scene, color, size, x, y, angle=0):
         super().__init__(scene)
         self.colorRect(color, size)
         self.x = x
@@ -13,46 +13,85 @@ class TestSprite(CollisionPhysics.CollisionPhysics):
         self.setAngle(angle)
         self.moveAngle = angle
         self.speed = 0
-        self.active = active
     
     def process(self):
-        # Allow for movement of first sprite only to test
-        if self.active:
-            if self.isKeyPressed(pygame.K_LEFT):
-                self.x -= 1
-            if self.isKeyPressed(pygame.K_RIGHT):
-                self.x += 1
-            if self.isKeyPressed(pygame.K_UP):
-                self.y -= 1
-            if self.isKeyPressed(pygame.K_DOWN):
-                self.y += 1
+        if self.isKeyPressed(pygame.K_LEFT):
+            self.x -= 1
+        if self.isKeyPressed(pygame.K_RIGHT):
+            self.x += 1
+        if self.isKeyPressed(pygame.K_UP):
+            self.y -= 1
+        if self.isKeyPressed(pygame.K_DOWN):
+            self.y += 1
         
         # Rotation
-            if self.isKeyPressed(pygame.K_q):
-                self.imageAngle += 1
-                self.moveAngle += 1
-            if self.isKeyPressed(pygame.K_e):
-                self.imageAngle -= 1
-                self.moveAngle -= 1
+        if self.isKeyPressed(pygame.K_q):
+            self.imageAngle += 1
+            self.moveAngle += 1
+        if self.isKeyPressed(pygame.K_e):
+            self.imageAngle -= 1
+            self.moveAngle -= 1
 
+class DriveSprite(CollisionPhysics.CollisionPhysics):
+    def __init__(self, scene, color, size, x, y, angle=0):
+        super().__init__(scene)
+        self.colorRect(color, size)
+        self.x = x
+        self.y = y
+        self.setAngle(angle)
+        self.moveAngle = angle
+        self.speed = 0
+    
+    def process(self):
+        if self.isKeyPressed(pygame.K_a):
+            self.x -= 1
+        if self.isKeyPressed(pygame.K_d):
+            self.x += 1
+        if self.isKeyPressed(pygame.K_w):
+            self.y -= 1
+        if self.isKeyPressed(pygame.K_s):
+            self.y += 1
+        
+        # Rotation
+        if self.isKeyPressed(pygame.K_n):
+            self.imageAngle += 1
+            self.moveAngle += 1
+        if self.isKeyPressed(pygame.K_m):
+            self.imageAngle -= 1
+            self.moveAngle -= 1
+
+class LblOut(simpleGE.Label):
+    def __init__(self):
+        super().__init__()
+        self.center = (320, 30)
+        self.size = (500, 30)
+        self.fgColor = "blue"
+        self.bgColor = "white"
+        self.clearBack = True
+                
 class CollisionScene(simpleGE.Scene):
     def __init__(self):
         super().__init__()
-        self.setCaption("SAT Collision Test")
+        self.background.fill("papayawhip")
+        self.setCaption("SAT Collision Test. WASD for blue, arrows for red. q (or e) to change red angle, n (or m) to change blue.")
         
-        self.sprite1 = TestSprite(self, "red", (120, 60), 300, 300, 30, active=True)
-        self.sprite2 = TestSprite(self, "blue", (100, 40), 500, 200, -20)
-        
-        self.sprites = [self.sprite1, self.sprite2]
+        self.sprite1 = MoveSprite(self, "red", (120, 60), 300, 300, 30)
+        self.sprite2 = DriveSprite(self, "blue", (100, 40), 500, 200, -20)
+        self.lblOut = LblOut()
+    
+        self.sprites = [self.sprite1, self.sprite2, self.lblOut]
         
     def update(self):
         super().update()
         
         testSAT = self.sprite1.collidesWithAdvanced(self.sprite2)
-        testSimple = self.sprite1.collidesWith(self.sprite2)
+        testAABB = self.sprite1.collidesWith(self.sprite2)
         
-        print(f" SAT: {testSAT} | Simple: {testSimple}")
+        self.lblOut.text = f"SAT: {testSAT} , AABB: {testAABB}"
+        
+def main():
+    game = CollisionScene()
+    game.start()
         
 if __name__ == "__main__":
-    scene = CollisionScene()
-    scene.start()
+    main()
